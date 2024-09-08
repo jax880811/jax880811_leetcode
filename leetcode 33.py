c60170085c36d0f94a1b29ac6f759a02642f28f5
jax880811 <jax880811@gmail.com>
2024-09-08 16:17:00 +0800

    leetcode 33

--- leetcode 33.py
+++ leetcode 33.py
@@ -24,3 +24,15 @@ nums = [4,5,6,7,0,1,2]
 target = 0
 solution = Solution()
 print(solution.search(nums,target))
+'''
+此題是比較特別的有序陣列
+0-7分割成4-7以及0-3,大的序列放到右邊去小的序列放到左邊
+也就是說利用二元搜尋法還必須要再加個條件
+若中間>target又大於最左邊元素值
+代表target在左半邊,於是讓right變成mid - 1
+若中間>target
+但是小於target,就是在右半邊
+left變成mid + 1
+已題目範例一為例
+Input: nums = [4,5,6,7,0,1,2], target = 0
+'''
