b3a235e30fc0f20e7456f3f3848f419cdd46adc4
jax880811 <jax880811@gmail.com>
2024-10-03 16:04:05 +0800

    2024/10/3

---
+++ leetcode 1046.py
@@ -0,0 +1,24 @@
+import heapq
+from typing import List
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while stones:
+            stones=sorted(stones,reverse=False)
+            s1 = stones.pop()
+            if not stones:
+                return s1
+            s2 = stones.pop()
+            if s1>s2:
+                stones.insert(len(stones),s1-s2)
+                
+
+
+        return 0
+    
+
+
+stones = [2,7,4,1,8,1]
+solution = Solution()
+print(solution.lastStoneWeight(stones))
+
