4f1c7a09e50ead45aa80b2794146843c8fafeff6
jax880811 <jax880811@gmail.com>
2024-11-14 22:37:31 +0800

    leetcode 55

--- dynamic programming/leetcode 55.py
+++ dynamic programming/leetcode 55.py
@@ -15,3 +15,11 @@ solution = Solution()
 
 print(solution.canJump(nums))
 
+'''
+題目的本質上是以第一步開始為基準點
+後面每個元素就是你能前進的最多步數
+如果能夠走到最後一步就回傳true
+不能救回傳false
+所以說先起始一個基準步數為answer
+往後一直走，如果answer小於計算的Index i代表著無法再繼續向前進，那就回傳false
+'''
