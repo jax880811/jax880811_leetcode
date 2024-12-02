a9452d7501a57863a9abd11080b5d913ae723472
jax880811 <jax880811@gmail.com>
2024-12-02 21:06:04 +0800

    leetcode 965

--- dfs&bfs/leetcode 463.py
+++ dfs&bfs/leetcode 463.py
@@ -19,11 +19,16 @@ class Solution:
             x,y = queue.popleft()
             for dx,dy in ([1,0],[0,1],[-1,0],[0,-1]):
                 nx,ny = x+dx,y+dy
-                if nx < 0 or nx >=len(grid) or ny<0 or ny >=len(grid[0]) or grid[nx][ny] !=1: #確認邊界
+                if nx < 0 or nx >=len(grid) or ny<0 or ny >=len(grid[0]) or grid[nx][ny] !=1: #確認邊界，確認周遭是否為陸地，若相鄰邊已經在邊界或者並非陸地，就計算邊
                     sum += 1
                 
-
         return sum
+
+'''
+此題要找出該陸地區域當中相鄰海域的邊角數為多少
+我這邊是先找出所有的陸地範圍
+接著在把每個陸地範圍的邊角都數出來
+'''
 
 
 grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
---
+++ dfs&bfs/leetcode 965.py
@@ -0,0 +1,73 @@
+from typing import List
+from typing import Optional
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+
+class Solution:
+    
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        key = root.val
+        def dfs(root):
+            if not root:
+                return
+            if root.val != key:
+                return False
+                
+            if dfs(root.left) is False:
+                return False
+            if dfs(root.right) is False:
+                return False
+            return True
+        
+        return dfs(root)
+
+
+
+def list_to_tree(values: List[Optional[int]]) -> Optional[TreeNode]:
+    if not values:  # 如果列表為空，返回 None
+        return None
+
+    # 創建根節點
+    root = TreeNode(values[0])
+    queue = [root]  # 使用佇列追蹤父節點
+    i = 1  # 從第二個元素開始
+
+    while i < len(values):
+        current = queue.pop(0)  # 取出父節點
+
+        # 建立左子節點
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        # 建立右子節點
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+
+    return root
+def print_tree_as_structure(node: Optional[TreeNode], level=0, prefix="Root: "):
+    """
+    以樹的形式打印二叉樹。
+    """
+    if not node:
+        return
+    print(" " * (level * 4) + prefix + str(node.val))
+    if node.left or node.right:  # 如果有子節點，繼續打印
+        print_tree_as_structure(node.left, level + 1, "L--- ")
+        print_tree_as_structure(node.right, level + 1, "R--- ")
+
+
+root = [2,2,2,5,2]
+tree_root = list_to_tree(root)
+print_tree_as_structure(tree_root)
+solution = Solution()
+print(solution.isUnivalTree(tree_root))
