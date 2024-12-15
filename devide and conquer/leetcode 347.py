a48c2a1696e96b355fa788f85859f053ad67bdd2
jax880811 <jax880811@gmail.com>
2024-12-15 17:30:00 +0800

    leetcode 347

--- devide and conquer/leetcode 347.py
+++ devide and conquer/leetcode 347.py
@@ -8,14 +8,20 @@ class Solution:
         
         heap = [(-freq, num) for num, freq in list_set.items()]
         heapq.heapify(heap)
-        
         heap = heapq.nsmallest(k,heap)
-        
+
         answer = []
         for i,j in heap:
             answer.append(j)
 
         return answer
+
+'''
+這題要取出該序列中出現最頻繁的值
+那麼先用記數的方式記錄每一筆元素的出現次數
+接著再用heap的方式尋找，使用-freq以及nsmallest則是為了運用最小堆的特性，元素會集中在前面幾格，省去往後找的麻煩
+取出最常出現的前k筆元素後再取出
+'''
 
 
 nums = [1,1,1,2,2,3]
