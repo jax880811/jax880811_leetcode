004af46c71dc94dc9e2183b646cd1ff23f0f6729
jax880811 <jax880811@gmail.com>
2025-02-07 22:54:38 +0800

    leetcode 237

---
+++ linked list/leetcode 237.py
@@ -0,0 +1,57 @@
+from typing import Optional
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+
+'''
+一開始我是有點理解錯題目要怎麼實作
+最一開始我是打算說遍歷所有的Listnode
+然後找到需要找的node之後
+再用node.val = node.next.val
+node.next = node.next.next
+
+沒想到這題目是直接這樣子做就可以了
+'''
+
+'''
+# 定義 ListNode 類別
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    # 為了方便打印，加入一個 __str__ 方法
+    def __str__(self):
+        result = []
+        current = self
+        while current:
+            result.append(str(current.val))
+            current = current.next
+        return " -> ".join(result)
+    
+def list_to_linkedlist(elements):
+    dummy = ListNode()
+    current = dummy
+    for element in elements:
+        current.next = ListNode(element)
+        current = current.next
+    return dummy.next
+
+class Solution:
+    def deleteNode(self, node):
+        
+
+        return 0
+
+
+head = list_to_linkedlist([4,5,1,9])
+node = 1
+solution = Solution()
+print(solution.deleteNode(head,node))
+'''
--- string/leetcode 383.py
+++ string/leetcode 383.py
@@ -3,13 +3,22 @@ from collections import Counter
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNote_count = Counter(ransomNote)
+        ransomNote_count = Counter(ransomNote)#記錄次串當中每個字元的出現次數
         magazine_count = Counter(magazine)
         for char,count in ransomNote_count.items():
-            if magazine_count[char] < count:
+            if magazine_count[char] < count: #如果magazine當中有任何一種字母數量少於ransomNote，代表無法從中組建出ransomNote，回報錯誤
                 return False
 
         return True
+
+'''
+這題是期待magazine這字串裡面的字元種類跟含量可不可以重新組出ransomnote的字串
+那麼其中一種方法就是利用counter的方式，計算兩個字串的字元種類跟總數
+如果magazine有特定字母數少於ransomnote，就代表無法使用magazine來去重新組出ransomnote
+回報錯誤
+
+時間複雜度: O(m + n)，其中m等於ransomnote數量，n等於magazine的字元數
+'''
 
 ransomNote = "aa"
 magazine = "aab"
