ebcdf579ea7d641488b340f881ae7dfa910dca20
jax880811 <jax880811@gmail.com>
2025-02-19 21:27:56 +0800

    leetcode 671

--- sorting/leetcode 88. Merge Sorted Array.py
+++ sorting/leetcode 88. Merge Sorted Array.py
@@ -4,9 +4,9 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         p1 = m-1
         p2 = n-1
-        pointer = m+n-1
-        while p2>=0:
-            if p1>=0 and nums1[p1]>nums2[p2]:
+        pointer = m+n-1 #指針只到nums1最後面
+        while p2>=0: 
+            if p1>=0 and nums1[p1]>nums2[p2]: #從nums1最後面的地方開始補數字，nums1的當前指針數比較大就補nums1 反之則是nums2
                 nums1[pointer] = nums1[p1]
                 p1 -= 1
             else:
@@ -15,7 +15,13 @@ class Solution:
             pointer -= 1
 
         return nums1
-    
+'''
+本題最佳解
+是從兩個陣列的最尾端，也就是兩陣列的最大值開始計算
+再併入原本的第一個陣列
+從第一個陣列最後面的地方開始補數字
+nums1的當前指針數比較大就補nums1 反之則是nums2
+'''
 
 nums1 = [1,2,3,0,0,0]
 m = 3
