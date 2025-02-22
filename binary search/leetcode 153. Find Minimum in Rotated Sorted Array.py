d1f58e9c1facf0a11d4c650ff07da3eba060043e
jax880811 <jax880811@gmail.com>
2025-02-22 21:50:35 +0800

    leetcode 153

--- binary search/leetcode 153. Find Minimum in Rotated Sorted Array.py
+++ binary search/leetcode 153. Find Minimum in Rotated Sorted Array.py
@@ -9,7 +9,7 @@ class Solution:
         right = len(nums)-1
         
         while left<right:
-            mid = left +(right-left)//2
+            mid = (left +right)//2
             if nums[mid] > nums[right]:
                 left = mid+1
             else:
