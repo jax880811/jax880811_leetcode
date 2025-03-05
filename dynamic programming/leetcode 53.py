70f19390caf5ec4f455055c88332051ee8eb79f8
jax880811 <jax880811@gmail.com>
2025-03-05 16:44:57 +0800

    leetcode 53在修正

--- dynamic programming/leetcode 53.py
+++ dynamic programming/leetcode 53.py
@@ -1,12 +1,17 @@
 from typing import List
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        current_sum = nums[0]
-        answer = nums[0]
-        for num in nums[1:]:
-            current_sum = max(num,current_sum+num)
-            answer = max(answer,current_sum)
-        return answer
+        if not nums:
+            return 0
+        sum = -float('inf')
+        temp = 0
+        for i in nums:
+            sum = max(sum,i,temp+i)
+            if i>(temp+i):
+                temp = i
+            else:
+                temp = temp+i
+        return sum
     
 nums = [-2,1,-3,4,-1,2,1,-5,4]
 solution = Solution()
